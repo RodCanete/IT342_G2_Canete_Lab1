93de4e0a5572d83d460face182f09682b9d331e8
Rod Gabrielle Cañete <98006488+RodCanete@users.noreply.github.com>
2026-02-16 20:00:02 +0800

    bug/fix for authentication

--- backend/userauth/src/main/java/com/canete/userauth/model/User.java
+++ backend/userauth/src/main/java/com/canete/userauth/model/User.java
@@ -1,18 +1,18 @@
 package com.canete.userauth.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "user")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
